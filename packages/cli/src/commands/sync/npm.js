d25fbbb4aa54020fb24b507aabd96672f8a744c6
Josh Black <josh@josh.black>
2020-03-29 22:01:37 -0500

    feat(cli): add support for npm

--- packages/cli/src/commands/sync/npm.js
+++ packages/cli/src/commands/sync/npm.js
@@ -13,21 +13,25 @@ const defaultIgnorePatterns = [
 function run(workspace) {
   const { packages } = workspace;
   return Promise.all(
-    packages.map(async ({ packagePath }) => {
-      const ignorePath = path.join(packagePath, '.npmignore');
-      const ignorePatterns = defaultIgnorePatterns.slice();
+    packages
+      .filter(({ packageJson }) => !packageJson.private)
+      .map(async ({ packageJson, packageFolder }) => {
+        const ignorePath = path.join(packageFolder, '.npmignore');
+        const ignorePatterns = defaultIgnorePatterns.slice();
 
-      if (await fs.pathExists(ignorePath)) {
-        const ignoreFile = await fs.readFile(ignorePath, 'utf8');
-        const localIgnorePatterns = ignoreFile.split('\n').filter((pattern) => {
-          return ignorePatterns.indexOf(pattern) === -1;
-        });
+        if (await fs.pathExists(ignorePath)) {
+          const ignoreFile = await fs.readFile(ignorePath, 'utf8');
+          const localIgnorePatterns = ignoreFile
+            .split('\n')
+            .filter((pattern) => {
+              return ignorePatterns.indexOf(pattern) === -1;
+            });
 
-        ignorePatterns.push(...localIgnorePatterns);
-      }
+          ignorePatterns.push(...localIgnorePatterns);
+        }
 
-      await fs.writeFile(ignorePath, ignorePatterns.join('\n'));
-    })
+        await fs.writeFile(ignorePath, ignorePatterns.join('\n'));
+      })
   );
 }
 
